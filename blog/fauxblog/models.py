a724f2f63b83794b41a6b503396887d5ca6328e9
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-09-05 19:45:32 +0300

    Close to final

---
+++ blog/fauxblog/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.contrib.postgres import search
+import datetime as dt
+
+# Create your models here.
+class Location(models.Model): 
+    location = models.CharField(max_length = 30, blank = False, null = False)
+
+    def __str__(self):
+        return self.location
+
+class Category(models.Model):
+    category = models.CharField(max_length = 30, blank = False, null = False)
+
+    def __str__(self):
+        return self.category
+
+class ImagePost(models.Model):
+    image_name = models.CharField(max_length = 30, blank = False)
+    image_description = models.TextField(blank = False, null = False)
+    image_location = models.ForeignKey(Location, on_delete = models.CASCADE)
+    image_category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    date_publshed = models.DateTimeField(auto_now_add = True)
+    posted_by = models.ForeignKey(User, on_delete = models.CASCADE,)
+    image = models.ImageField(upload_to = 'images/') 
+
+    def get_absolute_url(self):
+        return reverse('postdetails', args=(str(self.pk)))
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        pictures = cls.objects.filter(image_category__category__icontains = search_term)
+        return pictures
+

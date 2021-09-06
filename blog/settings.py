0b1daefebcc20b08e4f6d0544da544a4b97f6348
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-09-06 13:13:51 +0300

    Deployemnt10

--- blog/settings.py
+++ blog/settings.py
@@ -52,7 +52,7 @@ if config('MODE')=="dev":
 else:
    DATABASES = {
        'default': dj_database_url.config(
-           default=config('DATABASE2_URL')
+           default=config('DATABASE_URL')
        )
    }
 
